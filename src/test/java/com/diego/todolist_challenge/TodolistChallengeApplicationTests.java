118a4aaf3e4d7d0d1c6f25289863d9a8c765c4bf
DiegoCoelho <dccoelho93@gmail.com>
2025-01-27 20:06:19 -0300

    inicialização do projeto spring

---
+++ src/test/java/com/diego/todolist_challenge/TodolistChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.todolist_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
